48ac9d3968ada69594b3eab82e0aeb03059fca44
gangakailas <gangakailasnath@gmail.com>
2024-03-14 11:19:49 +0530

    first commit

---
+++ monmind/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Home} from "./components/home/Home.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ monmind/src/components/home/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Home.css'
+
+
+
+export const Home = () => {
+  return (
+    <div className="main"> 
+      <div className="icon">
+        <h2 className="logo">MoneyMinder</h2>
+      </div>
+
+      <div className="menu">
+        <button className="cn"><a href="#">SIGN UP</a></button>
+        <button className="cn"><a href="#">SIGN IN</a></button>
+      </div>
+      
+      <div className="content">
+        <h1>Finance Made Easier Than
+          <br/>Ever Before</h1>
+      </div>
+    </div>
+  );
+};
