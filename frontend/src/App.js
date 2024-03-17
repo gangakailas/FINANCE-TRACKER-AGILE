b6d618afd218f7f1f9b183feae8f49f4de1759c1
gangakailas <gangakailasnath@gmail.com>
2024-03-17 18:16:38 +0530

    signin done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import {Home} from "./components/home/Home.jsx"
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Home} from "./components/home/Home.jsx";
+import { Signin } from './components/signin/Signin';
+
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/signin" element={<Signin/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
